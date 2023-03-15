e4688da2ff0fa1b6f9a05b634a1b8bfa54f5f2f0
김덕현 <oraclian@estsecurity.com>
2023-03-15 17:28:36 +0900

    17장 기능 보강

--- dhkim/chapter_17/game/package.go
+++ dhkim/chapter_17/game/package.go
@@ -12,12 +12,14 @@ import (
 
 type Game struct {
 	randomNumber int
+	cnt          int
 }
 
 func NewGame() *Game {
 	seed := rand.NewSource(time.Now().UnixNano())
 	randomNumber := rand.New(seed).Intn(100)
-	return &Game{randomNumber}
+	cnt := 1
+	return &Game{randomNumber, cnt}
 }
 
 func (g *Game) Start() {
@@ -26,16 +28,22 @@ func (g *Game) Start() {
 	for {
 		fmt.Print("숫자를 입력하세요: ")
 		s, _ := reader.ReadString('\n')
-		n, _ := strconv.ParseInt(strings.TrimSpace(s), 10, 0)
+		n, err := strconv.ParseInt(strings.TrimSpace(s), 10, 0)
+		if err != nil {
+			fmt.Println("숫자가 아닌 값을 입력하셨습니다!!")
+			continue
+		}
+
 		r := g.Check(int(n))
 		if r == 0 {
-			fmt.Println("정답입니다!")
+			fmt.Println("정답입니다!", g.cnt, "번 만에 맞추셨습니다!")
 			break
 		} else if r == -1 {
 			fmt.Println("입력한 숫자보다 큽니다.")
 		} else {
 			fmt.Println("입력한 숫자보다 작습니다.")
 		}
+		g.cnt += 1
 	}
 }
 
